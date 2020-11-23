7023f8f8ce7b522064503ae976f0a25a8dcd5fc9
andersonlima07 <andersonlimasantos7498@hotmail.com>
2020-11-23 09:40:28 -0300

    Deploy da API

--- src/main/java/org/generation/blogPessoal/config/SwaggerConfig.java
+++ src/main/java/org/generation/blogPessoal/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis( RequestHandlerSelectors.basePackage
-						("generation.org.blogPessoal.controller") )
+						("org.generation.blogPessoal.controller") )
 				.paths(PathSelectors.any())
 				.build()
 				.apiInfo(apiInfo());
